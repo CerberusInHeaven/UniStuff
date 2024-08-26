9973a9e24d9739c1a78e0f3e7ca6f3524a9df1cc
NCerberus <nathanbandeirap@gmail.com>
2024-08-26 11:23:34 -0300

    26/08

--- pythonAlgs/stringManip/exercicios03/Exer1.py
+++ pythonAlgs/stringManip/exercicios03/Exer1.py
@@ -1,3 +1,12 @@
 #1. Elaborar um programa que leia uma senha e informe se ela é válida ou não. Para ser válida a senha
 #deve conter letras maiúsculas, minúsculas e números. Além disso, a senha deve possuir entre 8 e 12
 #caracteres.
+
+senha = "Senha1.."
+
+senha_try = input("Digite a senha do sistema")
+
+if senha.find(senha_try) >=0:
+    print("letra certa")
+else:
+    print("Letra não achada")
--- pythonAlgs/stringManip/test.py
+++ pythonAlgs/stringManip/test.py
@@ -2,7 +2,7 @@ palavra = "computador"
 
 letra = input("Letra: ")
 
-if palavra.find(letra) >= 0:
+if palavra.find(letra) >= (0, 5):
     print("Letra encontrada uwu")
 
 else:
