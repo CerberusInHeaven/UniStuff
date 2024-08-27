ebb5a319d3a3e903a14a69356090c020251909e8
Nathan Bandeira <nathanbandeirap@gmail.com>
2024-08-27 04:25:02 -0300

    27/06

--- pythonAlgs/stringManip/exercicios03/Exer1.py
+++ pythonAlgs/stringManip/exercicios03/Exer1.py
@@ -2,11 +2,35 @@
 #deve conter letras maiúsculas, minúsculas e números. Além disso, a senha deve possuir entre 8 e 12
 #caracteres.
 
-senha = "Senha1.."
+import time
 
-senha_try = input("Digite a senha do sistema")
 
-if senha.find(senha_try) >=0:
-    print("letra certa")
+senha = input("Sua senha: ")
+
+
+def validar_senha(senha):
+    if len(senha) <8 or len(senha) >12:
+     return False 
+    
+    possui_upper = False
+    possui_lower = False
+    possui_number = False
+
+    for letra in senha: 
+        if letra.isupper():
+            possui_upper = True
+        if letra.islower():
+            possui_lower = True
+        if letra.isdigit():
+            possui_number = True
+
+    return possui_upper and possui_lower and possui_number
+
+print("Checando viabilidade de sua senha...")
+time.sleep(3)
+
+if validar_senha(senha):
+    print("Senha válida, ta rodando patrão")
+   
 else:
-    print("Letra não achada")
+    print("Senha Invalidade, ela deve possui entre 8 a 12 caracteres e toda essas frescuras de segurança")
